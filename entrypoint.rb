0efc758e2a75ce2f5845b315f1890fc5107ad53e
Gerard Hickey <Gerard.Hickey@smartsheet.com>
2017-05-15 15:37:41 -0700

    Fixed references to PUPPET_FORGE vars in entrypoint script

--- entrypoint.rb
+++ entrypoint.rb
@@ -6,28 +6,28 @@ config = ["--log-dir=/puppet/logs"]
 # if the first arg starts with a dash, then user provided args
 unless ARGV[0] and ARGV[0].start_with? '-'
   # PUPPET_FORGE_PROXY="URL"
-  if ENV.key? "DOCKER_FORGE_PROXY"
-    config << "--proxy=#{ENV['DOCKER_FORGE_PROXY']}"
+  if ENV.key? "PUPPET_FORGE_PROXY"
+    config << "--proxy=#{ENV['PUPPET_FORGE_PROXY']}"
   end
 
   # PUPPET_FORGE_CACHE_TTL="SECS"
-  if ENV.key? "DOCKER_FORGE_CACHE_TTL"
-    config << "--ram-cache-ttl=#{ENV['DOCKER_FORGE_CACHE_TTL']}"
+  if ENV.key? "PUPPET_FORGE_CACHE_TTL"
+    config << "--ram-cache-ttl=#{ENV['PUPPET_FORGE_CACHE_TTL']}"
   end
 
   # PUPPET_FORGE_CACHE_SIZE="NUM"
-  if ENV.key? "DOCKER_FORGE_CACHE_SIZE"
-    config << "--ram-cache-size=#{ENV['DOCKER_FORGE_CACHE_SIZE']}"
+  if ENV.key? "PUPPET_FORGE_CACHE_SIZE"
+    config << "--ram-cache-size=#{ENV['PUPPET_FORGE_CACHE_SIZE']}"
   end
 
   # PUPPET_FORGE_MODULE_DIR="DIR[:DIR]"
-  if ENV.key? "DOCKER_FORGE_MODULE_DIR"
-    if ENV["DOCKER_FORGE_MODULE_DIR"].include? ':'
-      ENV["DOCKER_FORGE_MODULE_DIR"].split(":").each do |dir| 
+  if ENV.key? "PUPPET_FORGE_MODULE_DIR"
+    if ENV["PUPPET_FORGE_MODULE_DIR"].include? ':'
+      ENV["PUPPET_FORGE_MODULE_DIR"].split(":").each do |dir| 
         config << "--module-dir=#{dir}"
       end
     else
-      config << "--proxy=#{ENV["DOCKER_FORGE_MODULE_DIR"]}"
+      config << "--proxy=#{ENV["PUPPET_FORGE_MODULE_DIR"]}"
     end
   else 
     config << "--module-dir=/puppet/modules"
