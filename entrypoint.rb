c6bc23e6bcac06a988607d6c688810b5118d6c9f
Gerard Hickey <Gerard.Hickey@smartsheet.com>
2017-05-15 14:44:07 -0700

    Entrypoint for container

---
+++ entrypoint.rb
@@ -0,0 +1,54 @@
+#!/usr/bin/ENV ruby
+
+
+config = ["--log-dir=/puppet/logs"]
+
+# if the first arg starts with a dash, then user provided args
+unless ARGV[0] and ARGV[0].start_with? '-'
+  # PUPPET_FORGE_PROXY="URL"
+  if ENV.key? "DOCKER_FORGE_PROXY"
+    config << "--proxy=#{ENV['DOCKER_FORGE_PROXY']}"
+  end
+
+  # PUPPET_FORGE_CACHE_TTL="SECS"
+  if ENV.key? "DOCKER_FORGE_CACHE_TTL"
+    config << "--ram-cache-ttl=#{ENV['DOCKER_FORGE_CACHE_TTL']}"
+  end
+
+  # PUPPET_FORGE_CACHE_SIZE="NUM"
+  if ENV.key? "DOCKER_FORGE_CACHE_SIZE"
+    config << "--ram-cache-size=#{ENV['DOCKER_FORGE_CACHE_SIZE']}"
+  end
+
+  # PUPPET_FORGE_MODULE_DIR="DIR[:DIR]"
+  if ENV.key? "DOCKER_FORGE_MODULE_DIR"
+    if ENV["DOCKER_FORGE_MODULE_DIR"].include? ':'
+      ENV["DOCKER_FORGE_MODULE_DIR"].split(":").each do |dir| 
+        config << "--module-dir=#{dir}"
+      end
+    else
+      config << "--proxy=#{ENV["DOCKER_FORGE_MODULE_DIR"]}"
+    end
+  else 
+    config << "--module-dir=/puppet/modules"
+  end
+else
+  # append user provided args to the current config
+  config << ARGV
+  ARGV.clear
+  config.flatten!
+end
+
+case ARGV[0]
+when /README|HELP|INFO/i
+  exec "more README.md"
+when /VERSION/i
+  require_relative "lib/puppet_forge_server/version"
+  puts PuppetForgeServer::VERSION
+when nil
+  exec "ruby bin/puppet-forge-server #{config.join(' ')}"
+else
+  exec ARGV.join(' ')
+end
+
+
